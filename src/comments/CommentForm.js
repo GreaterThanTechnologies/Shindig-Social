cedfcc1fd74d91c5411470baeb83d6b9e4683ce4
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 11:15:34 -0400

    adds content to CommentForm.js from local file

---
+++ src/comments/CommentForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import addComment from '../actions/addComment'
+
+class CommentForm extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {content: ""}
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    const comment = {...this.state, comment_id: this.props.comment.id}
+    this.props.addComment(comment)
+    this.setState({content: ""})
+  }
+
+  handleChange(e) {
+      this.setState({[e.target.name]: e.target.value})
+  }
+
+
+  render() {
+
+    return (
+      <form className="comment___form" onSubmit={this.handleSubmit.bind(this)}>
+        <input className="commentForm___input" type="text" onChange={this.handleChange.bind(this)} value={this.state.content} name="content"/>
+
+        <input className="commentForm___submit" type="submit"/>
+      </form>
+  )
+  }
+
+}
+
+export default connect(null, {addComment})(CommentForm)
