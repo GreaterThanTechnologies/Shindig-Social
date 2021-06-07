513fb964359fab4dacd4e3e051d0e122a1ed8b94
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 11:00:24 -0400

    adds content to addPost.js from local file

---
+++ src/actions/addPost.js
@@ -0,0 +1,11 @@
+export default function addPost(post) {
+  return function(dispatch) {
+    fetch("http://localhost:3001/posts", {
+      method: "POST",
+      headers: {Accept: "application/json", "Content-Type": "application/json"},
+      body: JSON.stringify({post: post})
+    }).then(resp => resp.json())
+    .then(post => dispatch({type: "ADD_POST", payload: post}))
+  }
+
+}
