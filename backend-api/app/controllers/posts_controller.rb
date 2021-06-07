642c71edf99f268eaca48f94cb90fae9e6e07cfd
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 15:17:03 -0400

    adds post controller and code

--- backend-api/app/controllers/posts_controller.rb
+++ backend-api/app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+
+  def index
+    render json: Post.all, include: [comments: {only: [:content]}], except: [:created_at, :updated_at]
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post, include: [comments: {only: [:content]}], except: [:created_at, :updated_at]
+    else
+      render json: {message: post.errors.full_messages}
+    end
+  end
+
+  def update
+    post = Post.find_by(id: params[:id])
+    post.update(post_params)
+    render json: post, include: [comments: {only: [:content]}], except: [:created_at, :updated_at]
+  end
+
+  def destroy
+    post = Post.find_by(id: params[:id])
+    post.destroy
+    render json: {message: "successfully deleted!"}
+  end
+
+  #####
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
