b590aaed97d4b6e8ad93a85aea25ea5b141a9e5c
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 11:02:43 -0400

    adds content to deletePost.js from local file

---
+++ src/actions/deletePost.js
@@ -0,0 +1,11 @@
+export default function deletePost(postId) {
+
+  return function(dispatch) {
+
+    fetch(`http://localhost:3001/posts/${postId}`, {
+      method: "DELETE",
+      headers: {Accept: "application/json", "Content-Type": "application/json"},
+    }).then(resp => resp.json())
+    .then(post => dispatch({type: "DELETE_POST", payload: post.id}))
+  }
+}
