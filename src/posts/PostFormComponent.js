ea5be8c344f580a94150abd9b10f6c7245b7b183
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 12:04:57 -0400

    adds content to PostFormComponent.js from local file

---
+++ src/posts/PostFormComponent.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+class PostFormComponent extends React.Component {
+
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.submitCallback(this.props.post)
+  }
+
+  handleChange = (e) => {this.props.updateStateInParent({[e.target.name]: e.target.value})}
+
+  render = () => {
+
+    return (
+      <form className="postForm___form" onSubmit={this.handleSubmit.bind(this)}>
+        <input className="postForm___formInout" type="text" onChange={this.handleChange} value={this.props.post && this.props.post.name} name="name"/>
+
+        <input className="postForm___formSubmit" type="submit"/>
+      </form>
+  )
+  }
+
+}
+
+export default PostFormComponent
