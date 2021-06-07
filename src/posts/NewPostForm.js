752b0b5ad740d5b283fdf777200ef90d62220cc5
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 11:24:27 -0400

    adds content to NewPostForm.js from local file

---
+++ src/posts/NewPostForm.js
@@ -0,0 +1,37 @@
+import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {useState} from 'react'
+import addPost from '../actions/addPost'
+
+import PostFormComponent from './PostFormComponent'
+import React from 'react'
+
+class NewPostForm extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {name:  ""}
+  }
+
+  handleSubmit(post) {
+    this.props.addPost(post)
+    this.props.history.push('/posts')
+  }
+
+
+  handleChange = (post) => {this.setState({...this.state, ...post})}
+
+  render() {
+    return (
+      <PostFormComponent 
+        submitCallback={this.handleSubmit.bind(this)}
+        post={this.state}
+        updateStateInParent={this.handleChange.bind(this)}
+      />
+    )
+  }
+
+
+}
+
+export default withRouter(connect(null, {addPost})(NewPostForm))
