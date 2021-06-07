7dd22c170321d6864f5e50a8e4084ac1cb4f62a3
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 11:05:07 -0400

    adds content to editPost.js from local file

---
+++ src/actions/editPost.js
@@ -0,0 +1,16 @@
+export default function editPost(post) {
+
+
+  return function(dispatch) {
+
+    fetch(`http://localhost:3001/posts/${post.id}`, {
+      method: "PATCH",
+      headers: {Accept: "application/json", "Content-Type": "application/json"},
+      body: JSON.stringify({post: post})
+    }).then(resp => resp.json())
+    .then(post => dispatch({type: "EDIT_POST", payload: post}))
+
+  }
+
+
+}
