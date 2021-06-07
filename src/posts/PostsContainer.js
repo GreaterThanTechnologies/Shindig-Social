2c7c7b09d685648aab72eaecebc569744d3619df
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 12:13:46 -0400

    adds content to PostContainer.js from local file

---
+++ src/posts/PostsContainer.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import Posts from './Posts'
+import Post from './Post'
+import NewPostForm from './NewPostForm'
+import {Route, Switch} from 'react-router-dom'
+import {connect} from 'react-redux'
+import fetchPosts from '../actions/fetchPosts'
+
+class PostsContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+
+    render() {
+        return (
+          <div className="posts___cdm">
+            <Switch>
+              <Route path="/posts/new">
+                <NewPostForm className="newPost___form/>
+                </Route>
+              <Route exact path="/posts">
+                <Posts />
+                </Route>
+              <Route path='/posts/:id' render={(routerProps) => <Post {...routerProps}/>}/>
+            </Switch>
+          </div>
+        )
+    }
+
+}
+
+
+export default connect(null, {fetchPosts})(PostsContainer)
+
+
