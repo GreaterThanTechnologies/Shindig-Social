b3eb40e0e816ef560db29b509b83c5187e482766
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 15:18:33 -0400

    adds commentst controller and code

--- backend-api/app/controllers/comments_controller.rb
+++ backend-api/app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: CommentSerializer.new(comment)
+    else
+        render json: {message: comment.errors.full_messages}
+      end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id)
+  end
+
 end
