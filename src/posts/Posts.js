d6e2209449938b13495a62c30450f51492f9ff66
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-07 12:07:54 -0400

    adds content to Posts.js from local file

---
+++ src/posts/Posts.js
@@ -0,0 +1,33 @@
+import React, {Component, Fragment} from 'react'
+import Post from './Post'
+import PostForm from './PostForm'
+import {Redirect} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+
+
+
+class Posts extends Component {
+
+
+  render() {
+      console.log("render")
+      return (
+
+        <div className="posts___ulDiv">
+          <ul className="posts___ul">
+            {this.props.posts && this.props.posts.map((post, i) => <li key={post.id}><Link to={`/posts/${post.id}`} >{post.name}</Link></li> )}
+          </ul>
+        </div>
+      )
+  }
+
+}
+
+function mapStateToProps(state) {
+  console.log("mapStateToProps")
+  return {posts: state.posts}
+}
+
+
+export default connect(mapStateToProps)(Posts)
