c842522dbdfbe28c608c256313caecd79e9b7575
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-08 11:40:15 -0400

    addscss styling to sidebar

--- src/sideBar/SideBar.js
+++ src/sideBar/SideBar.js
@@ -1,13 +1,33 @@
 import {Link} from 'react-router-dom'
 import "./SideBar.css"
-
+import shindigLogo from '../images/shindigLogo.png'
+import shindigWords from '../images/shindigWords.png'
+import PostAddRoundedIcon from '@material-ui/icons/PostAddRounded';
+import SidebarOption from "./SidebarOption"
+import ChatBubbleOutlineRoundedIcon from '@material-ui/icons/ChatBubbleOutlineRounded';
 
 export default function SideBar(props) {
   return (
-      <div className="sidebar">
-          <Link to="/posts">Posts</Link>
-          <Link to="/posts/new">New Post</Link>
+
+    <div className="sidebar">
+      <img className="sidebar__logo" src={shindigLogo}
+        alt=""
+      />
+
+      <div className="sidebar__links">
+        <Link to="/posts" style={{textDecoration:"none"}}>
+          <SidebarOption Icon={PostAddRoundedIcon} text="Posts" />
+        </Link>
+
+        <Link to="/posts/new" style={{textDecoration:"none"}}>
+          <SidebarOption Icon={ChatBubbleOutlineRoundedIcon} text="New Post" />
+        </Link>
+
       </div>
+
+      <img className="sidebar__shindigWords" src={shindigWords} alt="" />
+
+    </div>
   )
 
 }
---
+++ src/sideBar/SidebarOption.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./SidebarOption.css"
+
+
+
+function SidebarOption({ active, text, Icon, src} ) {
+  return (
+    <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
+      <Icon />
+      {src}
+      <h2>{text}</h2>
+    </div>
+  )
+}
+export default SidebarOption
