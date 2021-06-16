f8c443226ce22b28b8e6b9ec933df3664f3a51c5
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-16 13:19:13 -0400

    cleans up reducer file

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,22 +1,21 @@
 export default function postsReducer(state = {posts: []}, action) {
 
   switch (action.type) {
-      case "ADD_POST":
-        return {posts: [...state.posts, action.payload]}
-      case "DELETE_POST":
-        return {posts: state.posts.filter(post => post.id !== action.payload)}
-      case "EDIT_POST":
-        const editedPostsArray = state.posts.map(post => post.id === action.payload.id ? action.payload : post)
-        return {posts: editedPostsArray}
-      case "FETCH_POSTS":
-        return {posts: action.payload}
-      case "ADD_COMMENT":
-        const updatedPost = state.posts.find(post => post.id === action.payload.id ? action.payload : post)
-        updatedPost.comments = [...updatedPost.comments, action.payload] 
-        return {posts: state.posts.map(post => post.id === updatedPost.id ? updatedPost : post)} 
-      default:
-      return state
-
+    case "ADD_POST":
+      return {posts: [...state.posts, action.payload]}
+    case "DELETE_POST":
+      return {posts: state.posts.filter(post => post.id !== action.payload)}
+    case "EDIT_POST":
+      const editedPostsArray = state.posts.map(post => post.id === action.payload.id ? action.payload : post)
+      return {posts: editedPostsArray}
+    case "FETCH_POSTS":
+      return {posts: action.payload}
+    case "ADD_COMMENT":
+      const updatedPost = state.posts.find(post => post.id === action.payload.id ? action.payload : post)
+      updatedPost.comments = [...updatedPost.comments, action.payload] 
+      return {posts: state.posts.map(post => post.id === updatedPost.id ? updatedPost : post)} 
+    
+      default: return state
   }
 }
 
