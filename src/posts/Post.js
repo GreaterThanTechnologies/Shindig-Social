b12ec2923a8637b0cc46654f22e9bdb745eff851
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-06-16 14:13:43 -0400

    corrects minor issue with rendering in app.cdd

--- src/posts/Post.js
+++ src/posts/Post.js
@@ -4,40 +4,42 @@ import deletePost from '../actions/deletePost'
 import CommentForm from '../comments/CommentForm'
 import Comment from '../comments/CommentContainer'
 import "../styles/Post.css"
+import { render } from '@testing-library/react'
 // stateless component #2
 
 export default function Post(props) {
 
-  const posts = useSelector(function(state) {return state.posts})
+  const posts = useSelector(function(state) {return state.posts});
   const post = posts.find(post => post.id === parseInt(props.match.params.id))
 
   const dispatch = useDispatch()
-  console.log(posts, post)
 
   const handleDelete = () => {
     dispatch(deletePost(post.id))
     props.history.push("/posts")
   }
 
-  return (
-    <>
-      <h1>
+  //render is not present, but when added, presents syntax error, missing semicolon
+  // render() {
+    return(
+      <>
+        <h1>
           {post && post.body}
-      </h1>
-      <div className="editPost__box">
-        <button 
-          className="editPost__button"
-          onClick={handleDelete}
-        >
-        Delete Post
-        </button>
-      </div>
-        <h3>Edit Post</h3><EditPostForm post={post}/>
-        <h3>Add Comment</h3><CommentForm post={post}/>
-        <h3>All Comments</h3>
-        {post && post.comments.map(comment => <Comment key={comment.id} body={comment}
-      />)}
-    </>
-  )
+        </h1>
+        <div className="editPost__box">
+          <button
+            className="editDelete__button"
+            onClick={handleDelete}
+            >
+            Delete Post
+            </button>
+        </div>
+          <h3>Edit Post</h3><EditPostForm post={post} />
+          <h3>Add Comment</h3><CommentForm post={post} />
+          <h3>All Comments</h3>
+          {post && post.comments.map(comment => <Comment key={comment.id} body={comment}
+          />)}
+      </>
+    )
+  // }
 }
-
